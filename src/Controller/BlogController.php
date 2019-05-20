13dda3de39f34ec427b85d696b9b22efb640441b
Noel AN <noel.an@hotmail.fr>
2019-05-20 09:20:32 +0200

    quete 09

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,86 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+	use Symfony\Component\HttpFoundation\Request;
+	use Doctrine\Common\Persistence\ObjectManager;
+	use App\Entity\Category;
+use App\Entity\Article;
+	
+
+class BlogController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="blog_index")
+    */
+    public function index()
+    {
+    	$categories = $this->getDoctrine()
+ 						   ->getRepository(Category::class)
+ 						   ->findAll();
+        return $this->render('/Blog/index.html.twig', ['categories' => $categories
+			]);
+    }
+
+    /**
+     * @Route("/blog/category/{name}", name="blog_category")
+     */
+    public function showByCategory(Category $category)
+    {
+    	// $category = $this->getDoctrine()
+ 				// 		 ->getRepository(Category::class)
+ 				// 		 ->findOneBy(['name' => mb_strtolower($Category)]);
+ 		$articles = $category->getArticles();
+
+ 		return $this->render('/Blog/category.html.twig', ['articles' => $articles,
+ 														  'category' =>$category
+			]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_new")
+     * @Route("/blog/edit/{id}", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager)
+    {
+
+    	if(!$article) {
+    	$article = new Article();
+    	}
+
+    	$categories = $this->getDoctrine()
+    					   ->getRepository(Category::class)
+    					   ->findAll();
+    	$form = $this->createFormBuilder($article)
+    				 ->add('title')
+    				 ->add('content')
+    				 ->add('category',ChoiceType::class, array(
+    				 	'choices' => $categories,
+    				 	'choice_label' => "name"
+    				 ))
+    				 ->getForm();
+
+ 	   	$form->handleRequest($request);
+ 	   	if ($form->isSubmitted() && $form->isValid()) 
+ 	   	{
+ 	   		$data = $form->getData();
+ 			$manager->persist($data);
+ 			$manager->flush();	
+
+ 		return $this->redirectToRoute('blog_category', ['categoryName' => $data->getCategory()->getName()]);
+ 	   	}
+
+ 		return $this->render('/Blog/create_article.html.twig', [
+ 				'formArticle' => $form->createView(),
+ 				// editMode 
+ 				'editMode' => $article->getId()
+ 		]);
+    }
+}
